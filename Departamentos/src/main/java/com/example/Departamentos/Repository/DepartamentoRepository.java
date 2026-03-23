16e399d0c84e0030befdf2615ace22839c0b16b0
JoaoGabrielMilare <168237403+JoaoGabrielMilare@users.noreply.github.com>
2026-03-22 21:46:00 -0300

    Criando o Repository

---
+++ Departamentos/src/main/java/com/example/Departamentos/Repository/DepartamentoRepository.java
@@ -0,0 +1,10 @@
+package com.example.Departamentos.Repository;
+
+import com.example.Departamentos.Model.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+
+}
